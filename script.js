3249e751336aa330f398c36a56d4f8c69cf0bf53
Aman Kumar <161146514+amann7974@users.noreply.github.com>
2024-08-12 16:16:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+// let view = document.getElementById('pageView');
+// let cost = document.getElementById('cost');
+// let slider = document.getElementById('price-slider');
+// let toggle = document.getElementById('billing');
+// let IsYearly = false;
+
+// let pageView = ["10K", "50K", "100K", "500K", "1M"];
+// let perMonth = [8, 12, 16, 24, 36];
+
+
+// slider.addEventListener('input', function(){
+//     updateValue();
+//     view.innerHTML = pageView[slider.value];
+
+//     let aman = this.value * 25;
+
+//     this.style.background = `linear-gradient(to right,
+//       hsl(174, 77%, 80%) 0px,
+//       hsl(174, 77%, 80%) ${aman}%,
+//       hsl(224, 65%, 95%) 0%,
+//       hsl(224, 65%, 95%) 100%`
+// });
+
+
+
+// toggle.addEventListener('change', function(){
+//     if(IsYearly == false)
+//     {
+//         IsYearly = true;
+//     }
+
+//     else
+//     {
+//         IsYearly = false;
+//     }
+// });
+
+// function updateValue()
+// {
+//     if(IsYearly)
+//     {
+//         cost.innerHTML = perMonth[slider.value] * .75
+//     }
+
+//     else
+//     {
+//         cost.innerHTML = perMonth[slider.value]
+//     }
+// }
+
+
+
+let a= Math.random();
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ style-guide.md
@@ -0,0 +1,42 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+> 💡 These are just the design sizes. Ensure content is responsive and meets WCAG requirements by testing the full range of screen sizes from 320px to large screens.
+
+## Colors
+
+### Primary
+
+- Soft Cyan (Full Slider Bar): 
+- Strong Cyan (Slider Backround): hsl(174, 86%, 45%)
+- Light Grayish Red (Discount Background): hsl(14, 92%, 95%)
+- Light Red (Discount Text): hsl(15, 100%, 70%)
+- Pale Blue (CTA Text): hsl(226, 100%, 87%)
+
+### Neutral
+
+- White (Pricing Component Background): hsl (0, 0%, 100%)
+- Very Pale Blue (Main Background): 
+- Light Grayish Blue (Empty Slider Bar): 
+- Light Grayish Blue (Toggle Background): hsl(223, 50%, 87%)
+- Grayish Blue (Text): 
+- Dark Desaturated Blue (Text & CTA Background): 
+
+## Typography
+
+### Body Copy
+
+- Font size (Introductory Paragraph): 15px
+
+### Font
+
+- Family: [Manrope](https://fonts.google.com/specimen/Manrope)
+- Weights: 600, 800
+
+> 💎 [Upgrade to Pro](https://www.frontendmentor.io/pro?ref=style-guide) for design file access to see all details and get hands-on experience using a professional workflow with tools like Figma.
